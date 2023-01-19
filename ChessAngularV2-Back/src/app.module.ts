ad6b261692c15f281ae286168562f2442aa1eb27
Darckdon-974 <hugo.feldodoux@gmail.com>
2023-01-19 14:34:39 +0100

    Creation de move dans le back-end

--- ChessAngularV2-Back/src/app.module.ts
+++ ChessAngularV2-Back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoveModule } from './move/move.module';
 
 @Module({
-  imports: [],
+  imports: [MoveModule],
   controllers: [AppController],
   providers: [AppService],
 })
